1bae9719c66810b2f354eb357991d7a9f578ed3e
Andrew Kanner <andrew.kanner@gmail.com>
2023-05-27 13:13:36 +0200

    task_09: samples/hello_world: add parameter to sleep before loading
    $ modprobe hello_world sleep_secs=2
    [   46.702584] Hello world!
    [   46.702974] sleeping 2 seconds ...
    $ modinfo hello_world
    filename:       /lib/modules/6.4.0-rc3-akanner-g53fd6df3ba88/kernel/samples/hello_world/hello-world.ko
    author:         Andrew Kanner <andrew.kanner@gmail.com>
    license:        GPL
    depends:
    retpoline:      Y
    intree:         Y
    name:           hello_world
    vermagic:       6.4.0-rc3-akanner-g53fd6df3ba88 SMP preempt mod_unload
    parm:           sleep_secs:sleep_secs (default=0) (int)
    $ modprobe -r hello_world
    [   91.122831] Bye-bye world!
    $ modprobe hello_world
    [   95.144469] Hello world!
    
    Signed-off-by: Andrew Kanner <andrew.kanner@gmail.com>

--- samples/hello_world/hello-world.c
+++ samples/hello_world/hello-world.c
@@ -5,10 +5,19 @@
  * Copyright (C) 2023 Andrew Kanner <andrew.kanner@gmail.com>
  */
 #include <linux/module.h>
+#include <linux/delay.h>
+
+static int sleep_secs;
+module_param(sleep_secs, int, 0644);
+MODULE_PARM_DESC(sleep_secs, "sleep_secs (default=0)");
 
 static int __init hello_world_init(void)
 {
 	pr_info("Hello world!\n");
+	if (sleep_secs) {
+		pr_info("sleeping %d seconds ...\n", sleep_secs);
+		msleep(sleep_secs * 1000);
+	}
 	return 0;
 }
 
